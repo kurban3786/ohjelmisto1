500535a1dd4ea8b9a5c7cfe2a7deff66fd05f725
kurban3786 <kurban3786@gmail.com>
2022-11-15 12:58:37 +0200

    commitjhbvjhvujvu

---
+++ tehtaav 3.1.py
@@ -0,0 +1,8 @@
+
+pitus = int(input("mitä cm kuhan pituuden on ?"))
+if pitus<37:
+    ero = 37-pitus
+    print(f"kuha on alamittainen,kuhan takaisin järveen ero on:{ero}")
+if pitus>=37:
+    print("onnea!")
+
